b7a1e354ca14c748aa2cda8eb0cd49fb18950296
Raya <46825220+mykukla@users.noreply.github.com>
2020-02-29 03:18:12 -0500

    Add files via upload

---
+++ ActivityInstruction Card/App.js
@@ -0,0 +1,318 @@
+//Just A Test
+import * as WebBrowser from 'expo-web-browser';
+import React from 'react';
+
+import {
+
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Alert,
+  Dimensions,
+
+} from 'react-native';
+
+import Swiper from "react-native-web-swiper";
+
+var { height } = Dimensions.get('window');
+var box_count = 3;
+var box_height = height / box_count;
+
+export default function HomeScreen() {
+  return (
+
+    <View style={styles.container}>
+      <ScrollView
+
+        style={styles.container}
+
+        contentContainerStyle={styles.contentContainer}>
+
+        <View style={[styles.box, styles.box1]}>
+
+
+
+          <View style={styles.titleContainer}>
+
+            <Text style={styles.titleText}>
+              Donation Instructions
+          </Text>
+          </View>
+
+          <View style={styles.howtoDonateContainer}>
+
+            <Text style={styles.howtoDonateText}>
+              To donate click on your desired organization, and select the
+              items you would like to send for donation.
+          </Text>
+
+            <View
+              style={[styles.codeHighlightContainer, styles.homeScreenFilename]}
+            />
+          </View>
+        </View>
+
+
+        <View style={[styles.box, styles.box2]}>
+          <View style={styles.ActivityTitleContainer}>
+            <Text style={styles.ActivityTitleText}>
+              Activity Board
+          </Text>
+          </View>
+
+          <View style={styles.ActivityContainer}>
+            <Text style={styles.ActivityListText}>
+              Activity 1 {'\n'}
+              Activity 2 {'\n'}
+              Activity 3 {'\n'}
+              Activity 4 {'\n'}
+              Activity 5 {'\n'}
+            </Text>
+          </View>
+        </View>
+        
+        {/*  ### NOT IMPLEMENTING THIS SECTION UNTIL NEXT SEMESTER! ### 
+
+        <View style={[styles.box, styles.box3]}>
+          <View style={styles.ItemsTitleContainer}>
+            <Text style={styles.ItemsTitleText}>
+              Items
+          </Text>
+          </View>
+          <View style={styles.itemsContainer}>
+            <Text style={styles.itemsText}>
+              Item 1   {"\n"}
+              Item 2    {"\n"}
+              Item 3    {"\n"}
+              Item 4    {"\n"}
+              Item 5    {"\n"}
+            </Text>
+            <Button
+              title="Add Items"
+              color=''
+              onPress={() => Alert.alert('The item has been added to your items.')}
+            />
+          </View>
+        </View>
+        */}
+
+        <View style={[styles.box, styles.box4]}>
+
+          <View style={styles.organizationsTitleContainer}>
+            <Text style={styles.organizationsTitleText}>
+              Organization Description
+          </Text>
+
+          </View>
+          <View style={styles.organizationsContainer}>
+            <Text style={styles.organizationsText}>
+              Donationplace Solution is a one stop shop to donate and get tax benefits.
+         </Text>
+            <View style={styles.organizationGallery}>
+            </View>
+            <View style={styles.organizationButton}>
+            </View>
+          </View>
+        </View>
+
+
+        <View style={styles.tabBarInfoContainer}>
+
+          <Text style={styles.tabBarInfoText}>
+            Tab Bar (Placeholder)
+        </Text>
+
+          <View
+            style={[styles.codeHighlightContainer, styles.navigationFilename]}
+          />
+        </View>
+      </ScrollView>
+    </View>
+
+
+  );
+}
+
+HomeScreen.navigationOptions = {
+  header: null,
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fffff',
+    flexDirection: 'column'
+
+  },
+
+  contentContainer: {
+
+  },
+  welcomeContainer: {
+    alignItems: 'center',
+
+  },
+
+
+  howtoDonateContainer: {
+
+    alignItems: 'center',
+
+  },
+
+  howtoDonateText: {
+
+    fontSize: 17,
+    color: 'black',
+    lineHeight: 27,
+    textAlign: 'center',
+    marginHorizontal: 50,
+    marginTop: 10,
+    marginBottom: 10,
+
+  },
+  tabBarInfoContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: '#8ebfbf',
+
+    ...Platform.select({
+      ios: {
+        //  shadowColor: 'black',
+        //shadowOffset: { width: 0, height: -3 },
+       // shadowOpacity: 0.1,
+       // shadowRadius: 3,
+      },
+      android: {
+        elevation: 20,
+      },
+    }),
+    alignItems: 'center',
+    backgroundColor: '#8ebfbf',
+    paddingVertical: 20,
+  },
+  tabBarInfoText: {
+    fontSize: 17,
+    color: '#ffffff',
+    textAlign: 'center',
+  },
+  navigationFilename: {
+    marginTop: 5,
+  },
+  ActivityTitleContainer: {
+
+    alignItems: 'center',
+    backgroundColor: '#c10543',
+    borderRadius: 3,
+    paddingHorizontal: 4,
+    
+
+  },
+  ActivityTitleText: {
+
+
+    color: 'white',
+    fontSize: 24,
+
+  },
+  titleContainer: {
+
+    alignItems: 'center',
+    backgroundColor: '#c10543',
+    borderRadius: 40,
+
+
+  },
+  titleText: {
+
+    color: 'white',
+    fontSize: 24,
+
+  },
+  ActivityContainer: {
+
+
+  },
+  ActivityListText: {
+    marginTop: 10,
+    marginBottom: 10,
+    fontSize: 17,
+    color: 'black',
+    lineHeight: 24,
+    textAlign: 'center',
+
+
+  },
+  ItemsTitleContainer: {
+
+    alignItems: 'center',
+    backgroundColor: 'black',
+  },
+  ItemsTitleText: {
+
+    color: 'white',
+    fontSize: 24,
+
+  },
+  itemsText: {
+    marginTop: 10,
+    marginBottom: 10,
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 24,
+    textAlign: 'center',
+
+
+  },
+  organizationsTitleContainer: {
+
+    alignItems: 'center',
+    backgroundColor: '#c10543',
+  },
+  organizationsTitleText: {
+    color: 'white',
+    fontSize: 24,
+  },
+  organizationsContainer: {
+
+  },
+  organizationsText: {
+    fontSize: 17,
+    color: 'black',
+    lineHeight: 24,
+    textAlign: 'center',
+    marginHorizontal: 50,
+    marginTop: 10,
+    marginBottom: 10,
+
+
+  },
+  itemsButton: {
+    marginHorizontal: 50,
+    color: '#ffffff',
+  },
+  organizationGallery: {
+    alignItems: 'center',
+  },
+  box: {
+    height: box_height
+  },
+  box1: {
+    backgroundColor: '#fff'
+
+  },
+  box2: {
+    backgroundColor: '#fff'
+  },
+  box3: {
+    backgroundColor: '#fffff'
+  },
+  box4: {
+    backgroundColor: '#fffff'
+  },
+});
---
+++ DonationCard Red-gradient:scrollbarApp.js/App.js
@@ -0,0 +1,143 @@
+import * as WebBrowser from 'expo-web-browser';
+import React from 'react';
+import Constants from 'expo-constants';
+import { LinearGradient } from 'expo-linear-gradient';
+
+import {
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Alert,
+  Dimensions,
+  Image,
+  Picker,
+  TouchableHighlight,
+} from 'react-native';
+
+import Swiper from 'react-native-web-swiper';
+
+var { height } = Dimensions.get('window');
+var box_count = 30;
+var box_height = height / box_count;
+
+export default function HomeScreen() {
+  createImages = () => {
+    let myimage = [];
+    for (let i = 0; i < 10; i++) {
+      myimage.push(
+        <TouchableHighlight
+          onPress={() => alert('select your donation type')}
+          underlayColor="white">
+          <Image
+            style={styles.logo}
+            source={{
+              uri:
+                'https://www.bbcgoodfood.com/sites/default/files/editor_files/2019/10/hydration-guide-main-image-700-350.jpg',
+            }}
+          />
+        </TouchableHighlight>
+      );
+    }
+    return myimage;
+  };
+
+  return (
+    <View style={styles.container}>
+      <LinearGradient
+        colors={['#c10e1b', '#FFFFFF']}
+        style={{
+          position: 'absolute',
+          left: 0,
+          right: 0,
+          top: 0,
+          height: Math.round(Dimensions.get('window').height),
+        }}
+      />
+      <ScrollView nativeID="OverAllVerticalScroll">
+        <View nativeID="titleContainer" style={styles.titleContainer}>
+          <Text nativeID="title" style={styles.mainTitle}>
+            Donation Place Solution
+          </Text>
+        </View>
+        <View nativeID="itemsContainer" style={styles.itemsContainer}>
+          <Text style={styles.itemTitle}>Items</Text>
+          <ScrollView nativeID="itemsImages" horizontal={true}>
+            {this.createImages()}
+          </ScrollView>
+        </View>
+        <View
+          nativeID="nonProfitOgranizationContainer"
+          style={styles.nonProfitOgranizationContainer}>
+          <Text>Non-Profit</Text>
+          <Text>Ogranizations</Text>
+          <ScrollView nativeID="nonProfitOgranizationImages" horizontal={true}>
+            {this.createImages()}
+          </ScrollView>
+        </View>
+        <View
+          nativeID="recentWorldNewsContainer"
+          style={styles.recentWorldNewsContainer}>
+          <Text>Weather-Related Disaster Areas In Need</Text>
+          <ScrollView nativeID="recentWorldNewsImages">
+            {this.createImages()}
+          </ScrollView>
+        </View>
+      </ScrollView>
+    </View>
+  );
+}
+
+HomeScreen.navigationOptions = {
+  header: null,
+};
+
+const styles = StyleSheet.create({
+  contentContainer: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  itemTitle: {
+    marginVertical: 5,
+  },
+  titleContainer: {
+    marginVertical: 10,
+  },
+  itemsContainer: {
+    marginVertical: 10,
+  },
+  nonProfitOgranizationContainer: {
+    flex: 1,
+    marginVertical: 10,
+  },
+  recentWorldNewsContainer: {
+    flex: 2,
+    marginVertical: 10,
+  },
+  container: {
+    flex: 3,
+    flexDirection: 'column',
+    paddingTop: Constants.statusBarHeight,
+    //backgroundColor: 'blue',
+    padding: 8,
+  },
+
+  mainTitle: {
+    textAlign: 'center',
+    // borderBottomWidth: StyleSheet.hairlineWidth,
+    fontSize: 30,
+  },
+  logo: {
+    resizeMode: 'cover',
+    borderRadius: 10,
+    height: 200,
+    width: 128,
+    paddingHorizontal: 25,
+    paddingVertical: 25,
+    marginHorizontal: 5,
+    marginVertical: 5,
+    aspectRatio: 0.75,
+  },
+});
